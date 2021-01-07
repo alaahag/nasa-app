437203dc82d1b4a2f8fc086896710734378f4e9e
Ala <alaahag@gmail.com>
2021-01-07 08:50:02 +0200

    fixed

--- server.js
+++ server.js
@@ -12,10 +12,13 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/^(?!.*/api/).*$/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, 'build')));
+    app.get('/^(?!.*/api/).*$/', function (req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+}
 
 app.use('/api', api);
 
